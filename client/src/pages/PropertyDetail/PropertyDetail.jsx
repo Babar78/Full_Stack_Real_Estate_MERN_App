1c2f4d433e8e922409be45374355799981de076c
Babar78 <muhammadbabarashraf8@gmail.com>
2024-01-20 21:11:54 +0500

    Get Residency Dataon Residency Details Page Created

--- client/src/pages/PropertyDetail/PropertyDetail.jsx
+++ client/src/pages/PropertyDetail/PropertyDetail.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import "./PropertyDetail"
+import React from "react";
+import "./PropertyDetail";
+
+import { PuffLoader } from "react-spinners";
+import { useQuery } from "react-query";
+import { useLocation } from "react-router-dom";
+import { getPropertyDetails } from "../../utils/api";
 
 const PropertyDetail = () => {
-  return (
-    <div className='wrapper'>PropertyDetail</div>
-  )
-}
+  // Get the Id from URL
+  const { pathname } = useLocation();
+  const id = pathname.split("/").slice(-1)[0];
 
-export default PropertyDetail
+  // Get Data From Backend
+  const { data, isError, isLoading } = useQuery(["residency", id], () =>
+    getPropertyDetails(id)
+  );
+
+  if (isLoading) {
+    return (
+      <div className="wrapper flexCenter" style={{ height: "60vh" }}>
+        <PuffLoader
+          height="80"
+          width="80"
+          radius="1"
+          color="#4066ff"
+          aria-label="loading..."
+        />
+      </div>
+    );
+  }
+  return <div className="wrapper">PropertyDetail</div>;
+};
+
+export default PropertyDetail;
