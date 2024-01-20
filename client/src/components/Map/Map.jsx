26bd4d69ca2bc6618b23d2cf565384ff015359b7
Babar78 <muhammadbabarashraf8@gmail.com>
2024-01-21 00:15:51 +0500

    Map Created on Details Page

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import GeoCoderMarker from "../GeoCoderMarker/GeoCoderMarker";
 
-const Map = () => {
+const Map = (props) => {
   return (
-     <div>
-        Map
-     </div>
-  )
-}
+    <MapContainer
+      center={[53.35, 18.8]}
+      zoom={1}
+      scrollWheelZoom={false}
+      style={{
+        height: "40vh",
+        width: "100%",
+        // marginTop: "20px",
+        zIndex: "0",
+        borderRadius:"6px"
+      }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <GeoCoderMarker
+        address={`${props.address} ${props.city} ${props.country}`}
+      />
+    </MapContainer>
+  );
+};
 
-export default Map
+export default Map;
