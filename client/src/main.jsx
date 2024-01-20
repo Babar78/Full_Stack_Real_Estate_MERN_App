0ae5498bb9e425d8b5a014b74439ad74c91cd06a
Babar78 <muhammadbabarashraf8@gmail.com>
2024-01-21 01:00:05 +0500

    Auth0 Added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { Auth0Provider } from "@auth0/auth0-react";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-3kzcm2psjq0mhye5.us.auth0.com"
+      clientId="EwoLkhb2ZpoMI9JgOzwIQ6w7JFSrLlkL"
+      authorizationParams={{
+        redirect_uri: "http://localhost:5173/",
+      }}
+      audience="http://localhost:8000"
+      scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
