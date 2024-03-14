ce9ef6b9aca29e80b4f6d5f5e47f292083f64156
Babar78 <muhammadbabarashraf8@gmail.com>
2024-03-14 19:16:46 +0500

    Searchbar's functionality added on Properties page

--- client/src/pages/Properties/Properties.jsx
+++ client/src/pages/Properties/Properties.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import "./Properties.css";
 import useProperties from "../../hooks/useProperties";
@@ -6,7 +6,11 @@ import { PuffLoader } from "react-spinners";
 import PropertyCard from "../../components/PropertyCard/PropertyCard";
 
 const Properties = () => {
+  // useState for searchbar
+  const [filter, setFilter] = useState("");
+
   const { data, isError, isLoading } = useProperties();
+
   if (isError) {
     return (
       <div className="wrapper">
@@ -32,11 +36,23 @@ const Properties = () => {
   return (
     <div className="wrapper">
       <div className="flexColCenter paddings innerWidth properties-container">
-        <SearchBar />
+        <SearchBar filter={filter} setFilter={setFilter} placeholder="Search by title/city/country..." />
         <div className="paddings flexCenter properties">
-          {data.map((card, index) => (
+          {/* {data.map((card, index) => (
             <PropertyCard card={card} key={index} />
-          ))}
+          ))} */}
+
+          {
+            data.filter((property) =>
+              property.title.toLowerCase().includes(filter.toLowerCase()) ||
+              property.city.toLowerCase().includes(filter.toLowerCase()) ||
+              property.country.toLowerCase().includes(filter.toLowerCase())
+            ).map(
+              (card, index) => (
+                <PropertyCard card={card} key={index} />
+              )
+            )
+          }
         </div>
       </div>
     </div>
